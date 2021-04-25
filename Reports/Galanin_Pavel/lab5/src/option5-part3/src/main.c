1d1b8047ed7597576db36ad01fee7899841b9cdd
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-04-25 19:33:51 +0300

    lab5/Galanin_Pavel : Updated rep&src option5 part3

---
+++ Reports/Galanin_Pavel/lab5/src/option5-part3/src/main.c
@@ -0,0 +1,265 @@
+#include <unistd.h> //read()
+#include <stdio.h>  //printf()
+#include <stdlib.h> //calloc(), realloc(), free()
+#include <string.h> //strcpy(), strlen()
+
+struct WordNode
+{
+    char* word;
+    struct WordNode* left;
+    struct WordNode* right;
+};
+struct WordNode* WordNode__Constructor(struct WordNode* object, char* word);
+struct WordNode* WordNode__Destructor(struct WordNode* object);
+struct WordNode* WordNode__add_top(struct WordNode* object, char* word);
+struct WordNode* WordNode__get_bottom(struct WordNode* object);
+struct WordNode* WordNode__get_by_index(struct WordNode* object, int index);
+void WordNode__print_word(struct WordNode* object);
+void WordNode__print_down_to_top(
+    struct WordNode* object,
+    void (*func)(struct WordNode* object)
+);
+struct WordNode* get_words_file_list(const char path[]);
+
+int main()
+{
+    struct WordNode* list_file_words = get_words_file_list("../lorem100.html");
+    //WordNode__print_down_to_top(list_file_words, WordNode__print_word);
+    struct WordNode* string_list = NULL;
+    struct WordNode* result = NULL;
+
+    const int buffer_size = 1;
+    char buffer[buffer_size];
+
+    int str_size = 0;
+    char* str = (char*) calloc(str_size, sizeof(char*));
+    int index_file_word = 0;
+    while(read(0, buffer, buffer_size))
+    {
+        char character = buffer[0];
+        if (character == '\n')
+        {
+            str_size += strlen(WordNode__get_by_index(list_file_words, index_file_word)->word);
+            str = (char*) realloc(str, str_size * sizeof(char));
+            index_file_word += 1;
+            strcat(str, WordNode__get_by_index(list_file_words, index_file_word)->word);
+
+            string_list = WordNode__add_top(string_list, str);
+
+            str_size = 0;
+            str = (char*) realloc(str, str_size * sizeof(char));
+
+            continue;
+        }
+        str_size += 1;
+        str = (char*) realloc(str, str_size * sizeof(char));
+        str[str_size - 1] = character;
+    }
+    free(str);
+
+    printf(" = = = = = Input = = = = =\n");
+    WordNode__print_down_to_top(string_list, WordNode__print_word);
+
+    for (struct WordNode* temp = WordNode__get_bottom(string_list); temp != NULL; temp = temp->right)
+    {
+        str_size = 0;
+        str = (char*) calloc(str_size, sizeof(char));
+        if (str == NULL)
+        {
+            printf("Память не выделилась\n");
+        }
+        //printf("word = %s\n", temp->word);
+
+        struct WordNode* words_list = NULL;
+        for (int i = 0; ; i++)
+        {
+            char ch = temp->word[i];
+            //printf("ch = %c\n", ch);  
+            if (ch == '\0') break;
+                      
+            if (ch == ' ')
+            {
+                //printf("Добавляется слово %s\n", str);
+                words_list = WordNode__add_top(words_list, str);
+                str_size = 0;
+                str = (char*) realloc(str, str_size * sizeof(char));
+                continue;
+            }
+            str_size += 1;
+            str = (char*) realloc(str, str_size * sizeof(char));
+            str[str_size - 1] = ch;
+        }
+        //WordNode__print_down_to_top(words_list, WordNode__print);
+        int number = atoi( WordNode__get_bottom(words_list)->word );
+        if( number != 0 && number % 2 == 0 )
+        {
+            result = WordNode__add_top(result, temp->word);
+            //printf(" = = = = = = = = = = %d\n", number);
+        }
+        words_list = WordNode__Destructor(words_list);
+        free(str);
+    }
+
+    string_list = WordNode__Destructor(string_list);
+
+    printf(" = = = = = Output = = = = =\n");
+    WordNode__print_down_to_top(result, WordNode__print_word);
+    result = WordNode__Destructor(result);
+
+    list_file_words = WordNode__Destructor(list_file_words);
+
+    return 0;
+}
+
+struct WordNode* WordNode__Constructor(struct WordNode* object, char* word)
+{
+    object = (struct WordNode*) malloc(sizeof(struct WordNode));
+    //printf("%p Constructor\n", object);
+    if (object == NULL)
+    {
+        printf("Не выделилась память\n");
+    }
+
+    object->left = NULL;
+    object->right = NULL;
+
+    object->word = (char*) calloc(strlen(word), sizeof(char));
+    if (object->word == NULL)
+    {
+        printf("Не выделилась память\n");
+    }
+    strcpy(object->word, word);
+
+    return object;
+}
+
+struct WordNode* WordNode__Destructor(struct WordNode* object)
+{
+    for (struct WordNode* temp = object; temp != NULL; )
+    {
+        free(temp->word);
+        object = temp;
+        //printf("%p Destructor\n", object);
+        temp = temp->left;
+        free(object);
+    }
+    return NULL;
+}
+
+struct WordNode* WordNode__add_top(struct WordNode* object, char* word)
+{
+    if (word == NULL)
+    {
+        return object;
+    }
+
+    if (word[0] == '\0')
+    {
+        return object;
+    }
+
+    struct WordNode* new_node = WordNode__Constructor(new_node, word);
+    new_node->left = object;
+    if (object == NULL)
+    {
+        return new_node;
+    }
+    
+    object->right = new_node;
+    return new_node;
+}
+
+struct WordNode* WordNode__get_bottom(struct WordNode* object)
+{
+    if (object == NULL)
+    {
+        return NULL;
+    }
+    struct WordNode* temp = object;
+    while(temp->left != NULL)
+    {
+        temp = temp->left;
+    }
+    return temp;
+}
+
+struct WordNode* WordNode__get_by_index(struct WordNode* object, int index)
+{
+    struct WordNode* temp = WordNode__get_bottom(object);
+    for(int i = 0; temp != NULL && i != index; temp = temp->right, i += 1)
+    {
+
+    }
+    return temp;
+}
+
+void WordNode__print(struct WordNode* object)
+{
+    printf("\t{\n");
+    printf("\t\t\"left\": \"%p\",\n", object->left);
+    printf("\t\t\"right\": \"%p\",\n", object->right);
+    printf("\t\t\"word\": \"%s\",\n", object->word);
+    printf("\t},\n");
+}
+
+void WordNode__print_word(struct WordNode* object)
+{
+    printf("%s\n", object->word);
+}
+
+void WordNode__print_down_to_top(struct WordNode* object, void (*func)(struct WordNode* object))
+{
+    printf("[\n");
+    for(struct WordNode* temp = WordNode__get_bottom(object); temp != NULL ; temp = temp->right)
+    {
+        func(temp);
+    }
+    printf("]\n");
+}
+
+struct WordNode* get_words_file_list(const char path[])
+{
+    FILE* file_pointer = fopen(path, "r");
+    if (file_pointer == NULL)
+    {
+        printf("File %s not opened\n", path);
+        return 0;
+    }
+
+    int word_size = 0;
+    char* word = (char*) calloc(word_size, sizeof(char));
+    struct WordNode* list_file_words = NULL;
+
+    while(!feof(file_pointer))
+    {
+        char character = fgetc(file_pointer);
+        //printf("%c", character);
+        if(
+            character == ' '
+            || character == ','
+            || character == '.'
+            || character == '!'
+        )
+        {
+            if (word_size == 0)
+            {
+                continue;
+            }
+            list_file_words = WordNode__add_top(list_file_words, word);
+            word_size = 0;
+            word = (char*) realloc(word, word_size * sizeof(char));
+            continue;
+        }
+        word_size += 1;
+        word = (char*) realloc(word, word_size * sizeof(char));
+        word[word_size - 1] = character;
+    }
+
+    if (word != NULL)
+    {
+        free(word);
+    }
+    fclose(file_pointer);
+
+    return list_file_words;
+}
